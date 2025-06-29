c3ae07ffc66e525d29a8d6b2ca2f734f53ce3714
Ankit Das <ankitsonu240@gmail.com>
2025-06-29 19:26:04 +0530

    order schema and controllers are added.

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import moongoose from "mongoose";
+import catchAsync from "../utils/errorCatching";
+import Order from "../schema/order.schema";
+import { order } from "../types/order.types";
+
+export const createOrder = catchAsync(async (req: Request, res: Response) => {
+  let {
+    userId,
+    firstName,
+    lastName,
+    address,
+    city,
+    country,
+    zipCode,
+    totalAmount,
+    items,
+  } = req.body as order; 
+
+  userId = new moongoose.Types.ObjectId(userId);
+  items = items.map(
+    (product_Id) => new moongoose.Types.ObjectId(product_Id)
+  );
+
+  const orderDetails = {
+    userId,
+    firstName,
+    lastName,
+    address,
+    city,
+    country,
+    zipCode,
+    totalAmount,
+    items,
+  };
+
+  const orderCreated = await Order.create({ ...orderDetails });
+
+  res.status(201).json(orderCreated);
+});
+
+export const getAllOrders = catchAsync(async (req: Request, res: Response) => {
+  const allOrders = await Order.find({});
+  res.status(200).json(allOrders);
+});
---
+++ src/routes/order.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { createOrder, getAllOrders } from "../controllers/order.controller"
+
+const router = express.Router();
+
+router.get("/", getAllOrders);
+router.post("/create",createOrder);
+
+export default router;
