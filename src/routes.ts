43b6ad8465fa6d7b2222e0c04155dcac57bbe4dc
Ankit Das <ankitsonu240@gmail.com>
2025-06-29 19:32:34 +0530

    made some changes in routes.

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import productRoutes from './routes/product.routes';
-import testingRoutes from './routes/test.routes';
+import userRoutes from "./routes/user.routes";
+import orderRoutes from "./routes/order.routes";
 
 const router = express.Router();
 
-router.use("/test", testingRoutes);
 router.use("/products", productRoutes);
+router.use("/users", userRoutes);
+router.use("/orders", orderRoutes);
     
 export default router;
