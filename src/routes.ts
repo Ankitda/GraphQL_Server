c6c7df795879e9e989fb75b48ee5099ea001196a
Ankit Das <ankitsonu240@gmail.com>
2025-08-13 21:59:22 +0530

    Added Auth routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import express from "express";
 import productRoutes from "./routes/product.routes";
 import userRoutes from "./routes/user.routes";
 import orderRoutes from "./routes/order.routes";
+import authRoutes from "./routes/auth.routes";
 
 const router = express.Router();
 
-router.use("/products", productRoutes);
+router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/products", productRoutes);
 router.use("/orders", orderRoutes);
 
 export default router;
