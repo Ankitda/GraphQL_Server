c96798e2b60e0796533cb0cf07c2a7d59e72448d
Ankit Das <ankitsonu240@gmail.com>
2025-08-14 22:05:53 +0530

    Added jwt middleware for authorization.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import router from "./routes";
+import { verifyJwtToken } from "./middlewares/jwtTokenVerifier";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(verifyJwtToken);
 
 // Routes
 app.use("/api", router);
