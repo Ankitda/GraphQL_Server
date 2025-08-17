b4e063356419d0eae518a241156e9fd5e6539aca
Ankit Das <ankitsonu240@gmail.com>
2025-08-17 21:27:31 +0530

    Added Order, User and Product field constants

---
+++ src/constants/docFeilds.constants.ts
@@ -0,0 +1,69 @@
+export const userFields = [
+  "_id",
+  "username",
+  "email",
+  "password",
+  "isActive",
+  "orders",
+  "createdAt",
+  "updatedAt",
+];
+
+export const productFields = [
+  "_id",
+  "title",
+  "description",
+  "price",
+  "discountPercentage",
+  "rating",
+  "totalReviews",
+  "stock",
+  "brand",
+  "category",
+  "subCategory",
+  "thumbnail",
+  "images",
+  "status",
+  "metadata",
+  "isActive",
+  "createdAt",
+  "updatedAt",
+];
+
+export const orderFields = [
+  "orderNumber",
+  "userDetails",
+  "shippingAddress",
+  "billingAddress",
+  "items",
+  "payment",
+  "status",
+  "subtotal",
+  "tax",
+  "shippingCost",
+  "discount",
+  "totalAmount",
+  "notes",
+  "trackingNumber",
+  "estimatedDeliveryDate",
+  "cancelReason",
+  "createdAt",
+  "updatedAt",
+];
+
+export const orderAddressFields = [
+  "street",
+  "city",
+  "state",
+  "country",
+  "zipCode",
+];
+
+export const orderItemFields = ["product", "quantity", "discount", "subtotal"];
+
+export const orderPaymentFields = [
+  "method",
+  "status",
+  "transactionId",
+  "paidAt",
+];
