0d832fb816798cc1eaa140424d421ebe2a7923a6
Ankit Das <ankitsonu240@gmail.com>
2025-09-04 20:10:42 +0530

    Added Verification Routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import productRoutes from "./routes/product.routes";
 import userRoutes from "./routes/user.routes";
 import orderRoutes from "./routes/order.routes";
 import authRoutes from "./routes/auth.routes";
+import verificationRoutes from "./routes/verification.routes";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
 router.use("/orders", orderRoutes);
+router.use("/verification", verificationRoutes);
 
 export default router;
