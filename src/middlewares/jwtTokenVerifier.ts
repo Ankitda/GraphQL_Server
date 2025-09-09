63a13e2bd989eab087f077bb15bd69a0e521652b
Ankit Das <ankitsonu240@gmail.com>
2025-09-09 21:52:59 +0530

    Modified JWT Token middleware to add user in request from database

--- src/middlewares/jwtTokenVerifier.ts
+++ src/middlewares/jwtTokenVerifier.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import ApiError from "../utils/apiError";
 import { IUser } from "../types/user.types";
+import User from "../schema/user.schema";
+import errorWrapper from "../utils/errorCatching";
 
 declare global {
   namespace Express {
@@ -11,25 +13,27 @@ declare global {
   }
 }
 
-export const verifyJwtToken = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const paths = ["/graphql", "/api/auth/login", "/api/users/create"];
-  if (paths.includes(req.path)) return next();
+export const verifyJwtToken = errorWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paths = ["/graphql", "/api/auth/login", "/api/users/create"];
+    if (paths.includes(req.path)) return next();
 
-  const token = req.headers.authorization?.split(" ")[1];
+    const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) {
-    throw new ApiError("No token provided", 401);
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
-    if (err) {
-      throw new ApiError("Invalid token", 401);
+    if (!token) {
+      throw new ApiError("No token provided", 401);
     }
-    req.user = decoded as IUser;
-    next();
-  });
-};
+
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+      async (err, decoded: any) => {
+        if (err) {
+          throw new ApiError("Invalid token", 401);
+        }
+        req.user = (await User.findById(decoded._id)) as IUser;
+        next();
+      }
+    );
+  }
+);
