5bef6049a44f769dc9902de2daee51903a6db0c0
Ankit Das <ankitsonu240@gmail.com>
2025-09-15 22:12:51 +0530

    Updated the order routes

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,9 +1,20 @@
 import express from "express";
-import { createOrder, getAllOrders } from "../controllers/order.controller";
+import {
+  createOrder,
+  getAllOrders,
+  getOrdersByStatus,
+  getOrderById,
+  updateOrder,
+} from "../controllers/order.controller";
+import { isPhoneVerified } from "../middlewares/order.middleware";
+import { isAccountVerified } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
-router.get("/", getAllOrders);
-router.post("/create", createOrder);
+router.post("/create", isAccountVerified, isPhoneVerified, createOrder);
+router.put("/:ordernumber", isAccountVerified, isPhoneVerified, updateOrder);
+router.get("/all", getAllOrders);
+router.get("/:ordernumber", getOrderById);
+router.get("/status/:status", getOrdersByStatus);
 
 export default router;
