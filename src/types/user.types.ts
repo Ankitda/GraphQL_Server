2878c719b3883f5de44c909afde7916f51052dad
Ankit Das <ankitsonu240@gmail.com>
2025-10-26 19:00:00 +0530

    Enhanced the user type according to the schema

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,6 +1,22 @@
-export interface User{
-    username : string;
-    email :string;
-    password : string;
-    isAdmin? : boolean;
+import { Types } from "mongoose";
+
+export interface IOrder {
+  orderId: Types.ObjectId;
+  orderNo: string;
+}
+export interface IUser {
+  _id?: string;
+  username: string;
+  email: string;
+  password: string;
+  phoneNo: string;
+  isPhoneNoVerified: boolean;
+  resetPasswordToken: string;
+  resetPasswordExpires: number;
+  accountVerified: boolean;
+  role: "USER" | "ADMIN";
+  isActive: boolean;
+  orders?: IOrder[]; // array of orders
+  createdAt?: Date;
+  updatedAt?: Date;
 }
