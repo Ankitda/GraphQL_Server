44ff68e562e5a099bc06bb1924980c0d226a25bb
Ankit Das <ankitsonu240@gmail.com>
2025-10-31 21:56:13 +0530

    Given Typedefs for the query functions

--- src/graphQl/types/user.types.ts
+++ src/graphQl/types/user.types.ts
@@ -1,14 +1,35 @@
 import { gql } from "apollo-server-express";
 
 export const userTypeDefs = gql`
+  scalar Date
+
+  enum Role {
+    USER
+    ADMIN
+  }
+
+  type IOrder {
+    orderId: ID!
+    orderNo: String!
+  }
+
   type User {
     _id: ID!
     username: String!
     email: String!
-    isAdmin: Boolean!
+    phoneNo: String!
+    role: Role!
+    accountVerified: Boolean!
+    isPhoneNoVerified: Boolean!
+    isActive: Boolean!
+    orders: [IOrder]!
+    createdAt: Date!
+    updatedAt: Date!
   }
 
   type Query {
-    users: [User!]!
+    users(limit: Int, offset: Int): [User!]!
+    user(id: String!): User!
+    userOrderHistory(id: String!): [IOrder!]!
   }
 `;
