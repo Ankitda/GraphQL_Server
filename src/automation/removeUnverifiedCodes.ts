609c5b0c053627f28bd9a96a4f9dc5876952e17d
Ankit Das <ankitsonu240@gmail.com>
2026-01-03 21:29:15 +0530

    Fixed the invalid cron expression in removeUnverifiedCodes.

--- src/automation/removeUnverifiedCodes.ts
+++ src/automation/removeUnverifiedCodes.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import Verification from "../schema/verification.schema";
 
 export const removeUnverifiedCodes = () => {
-  cron.schedule("*30 * * * *", async () => {
+  cron.schedule("*/1440 * * * *", async () => {
     const tenMinutesAgo = Date.now() - 10 * 60 * 1000;
     await Verification.deleteMany({
       createdAt: { $lt: new Date(tenMinutesAgo) },
