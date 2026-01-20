28ab2403138944f43ed52e89a7655f41e53ca51d
Ankit Das <ankitsonu240@gmail.com>
2026-01-20 20:21:46 +0530

    Added : Two new Routes were added in user module

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,6 +5,8 @@ import {
   getAllUsers,
   getOrdersByUser,
   updateUser,
+  requestAccountDeactivation,
+  cancelAccountDeactivation,
 } from "../controllers/user.controller";
 import { isAccountVerified } from "../middlewares/auth.middleware";
 
@@ -15,5 +17,7 @@ router.get("/find/order", getOrdersByUser);
 router.get("/all", getAllUsers);
 router.post("/create", createUser);
 router.put("/update/user", isAccountVerified, updateUser);
+router.post("/deactivate/request", isAccountVerified, requestAccountDeactivation);
+router.post("/deactivate/cancel", isAccountVerified, cancelAccountDeactivation);
 
 export default router;
